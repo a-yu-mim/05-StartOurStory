7262d1a223da880a953e89ebb6635acfc4d59e7d
Jay27032000 <julia.yamasaki@sptech.school>
2026-05-04 08:38:49 -0300

    auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,9 +1,14 @@
 
-function verificarSessao() {
+function validarSessao() {
     if (!sessionStorage.ID_USUARIO) {
         alert("Você precisa estar logado para acessar esta página.");
-        window.location.href = "/pages/login.html";
+        window.location.href = "/pages/cadastro.html";
     }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location.href = "/pages/login.html";
 }
 
 verificarSessao();
