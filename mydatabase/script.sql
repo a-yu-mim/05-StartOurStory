f5511cf9eab8a488a88d64c053faa3244107a3c5
Jay27032000 <julia.yamasaki@sptech.school>
2026-05-13 23:38:50 -0300

    atualizando banco de dados

--- mydatabase/script.sql
+++ mydatabase/script.sql
@@ -3,43 +3,47 @@ USE StartOurStory;
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(45) UNIQUE NOT NULL,
-	email VARCHAR(45) UNIQUE NOT NULL,
-	senha VARCHAR(45) NOT NULL,
-	fkParceiro INT NULL,
-	CONSTRAINT fkUsuarioParceiro 
+	nome VARCHAR(45) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	senha VARCHAR(100) NOT NULL,
+    fkParceiro INT UNIQUE NULL,
+    CONSTRAINT fkUsuarioParceiro 
 		FOREIGN KEY (fkParceiro) REFERENCES usuario(id)
 );
 
 CREATE TABLE codigo (
-	codigo INT PRIMARY KEY AUTO_INCREMENT,
-	fkUsuario INT NOT NULL,
-	FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+	codigo INT AUTO_INCREMENT,
+	fkUsuario INT,
+    horario DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (codigo, fkUsuario),
+	CONSTRAINT fkCodigoUsuario
+		FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
 ) AUTO_INCREMENT = 1000;
-
-CREATE TABLE casamento (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dataCasamento DATE,
-	horario TIME,
-	fkUsuario1 INT NULL,
-	fkUsuario2 INT NULL,
-	CONSTRAINT fkCasamentoUsuario1
-		FOREIGN KEY (fkUsuario1) REFERENCES usuario(id),
-	CONSTRAINT fkCasamentoUsuario2
-		FOREIGN KEY (fkUsuario2) REFERENCES usuario(id)
-);
 
 CREATE TABLE convidado (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(45),
+	nome VARCHAR(45) NOT NULL,
+    horario DATETIME DEFAULT CURRENT_TIMESTAMP,
 	fkUsuario INT,
-	UNIQUE (nome, fkUsuario),
-	FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+    UNIQUE (nome, fkUsuario),
+    CONSTRAINT fkConvidadoUsuario
+		FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
 );
 
 CREATE TABLE economia (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	valor DECIMAL(7,2),
+	valor DECIMAL(10,2) NOT NULL,
+    horario DATETIME DEFAULT CURRENT_TIMESTAMP,
 	fkUsuario INT,
-	FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+    CONSTRAINT fkEconomiaUsuario
+		FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE minigame (
+	id INT AUTO_INCREMENT,
+	caminho VARCHAR(100) NOT NULL,
+	fkUsuario INT,
+	PRIMARY KEY (id, fkUsuario),
+	CONSTRAINT fkMinigameUsuario
+		FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
 );
