a53d936663a17ccf8069afef66abb91c9454dac6
Jay27032000 <julia.yamasaki@sptech.school>
2026-05-24 01:32:57 -0300

    estilização da index e login

--- public/js/login.js
+++ public/js/login.js
@@ -38,30 +38,36 @@ function login() {
     .then(function (dadosUsuario){
         if (!dadosUsuario) return;
 
-        mostrarPorta();
+        document.getElementById("input_email").value = "";
+        document.getElementById("input_senha").value = "";
 
         sessionStorage.setItem("ID_USUARIO",    dadosUsuario.id);
         sessionStorage.setItem("EMAIL_USUARIO", dadosUsuario.email);
         sessionStorage.setItem("NOME_USUARIO",  dadosUsuario.nome);
+        mostrarAlerta("Login realizado com sucesso!", "green");
+        abrirPorta();
     })
     .catch(function (erro) {
-        mostrarAlerta("Não foi possivel concluir a operação.", "black");
+        mostrarAlerta("Erro desconhecido.", "black");
     });
 }
 
-function mostrarPorta() {
-    let porta = document.getElementById("porta");
-    let somPorta = document.getElementById("somPorta");
+function abrirPorta() {
+    let imagem = document.querySelector(".porta img");
+    let alavanca = document.getElementById("alavanca");
 
-    if (porta) {
-        porta.style.display = "block";
-    }
-
-    if (somPorta) {
-        somPorta.play();
+    if(alavanca.style.left == "32px"){
+        imagem.src = "../assets/porta_aberta_dark.png";
+        imagem.style.cursor = "pointer";
+        imagem.onclick = irParaHome;
+        return;
+    } else {
+        imagem.src = "../assets/porta_aberta.png";
+        imagem.style.cursor = "pointer";
+        imagem.onclick = irParaHome;
+        return;
     }
 }
-
 
 function mostrarAlerta(mensagem, cor) {
     let alerta   = document.getElementById("alerta");
@@ -77,54 +83,6 @@ function fecharAlerta() {
 
     if (alertaEl) {
         alertaEl.style.display = "none";
-    }
-}
-    
-function darkModo() {
-    let fundo = document.getElementById("body");
-    let botao = document.getElementById("botao");
-    let alavanca = document.getElementById("alavanca");
-    let card = document.querySelector(".card");
-    let h2 = document.querySelector(".card h2");
-    let p = document.querySelector(".card p");
-    let link = document.getElementById("link");
-    let email = document.getElementById("input_email");
-    let senha = document.getElementById("input_senha");
-    let btnEntrar = document.getElementById("btnEntrar");
-
-    if (alavanca.style.left == "32px") {
-        botao.style = "";
-        alavanca.style = "";
-        fundo.style = "";
-        card.style = "";
-        h2.style = "";
-        p.style = "";
-        link.style = "";
-        btnEntrar.style = "";
-        email.style = "";
-        senha.style = "";
-      } else {
-        botao.style.background = "#ccc";
-        alavanca.style.left = "32px";
-        alavanca.style.background = "#0c0c0c";
-        alavanca.style.boxShadow = "0 2px 6px rgba(0, 0, 0, 0.3)";
-        fundo.style.background = "#323232";
-        card.style.background = "#141414";
-        card.style.boxShadow = "0 2px 10px rgba(0, 0, 0, 0.3)";
-        card.style.border = "2px solid #2a2a2a";
-        card.style.color = "#f5f5f5";
-        card.style.boxShadow = "0 2px 10px rgba(0, 0, 0, 0.3)";
-        h2.style.color = "#ffffff";
-        p.style.color = "#838383";
-        link.style.color = "#838383";
-        btnEntrar.style.color = "#ffffff";
-        btnEntrar.style.background = "#0a0a0a";
-        email.style.background = "#141414";
-        email.style.color = "#ffffff";
-        email.style.border = "2px solid #2f2f2f";
-        senha.style.background = "#141414";
-        senha.style.color = "#ffffff";
-        senha.style.border = "2px solid #2f2f2f";
     }
 }
 
